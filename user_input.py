7c81471e58a65ab2ab645782eef9a0b7f907f8db
muthomi <mikebluez196@gmail.com>
2024-02-22 10:59:48 +0300

    This is a basic input output interface to collect user data.

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name=input('What is your name:')
+age=input('what is your age:')
+location=input('where do you reside:')
+print('Hello {} I hope all is well. You are {}yrs old and live in {}'.format(name,age,location))
